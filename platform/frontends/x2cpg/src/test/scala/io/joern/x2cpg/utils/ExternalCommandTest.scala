6fa370dfd6010e873513e7bf690c73fef6706b07
Prabhu Subramanian <prabhu@appthreat.com>
2023-09-22 17:02:47 +0100

    Fix test to make it cross platform
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- platform/frontends/x2cpg/src/test/scala/io/joern/x2cpg/utils/ExternalCommandTest.scala
+++ platform/frontends/x2cpg/src/test/scala/io/joern/x2cpg/utils/ExternalCommandTest.scala
@@ -8,19 +8,19 @@ import scala.util.{Failure, Success}
 
 class ExternalCommandTest extends AnyWordSpec with Matchers {
   "ExternalCommand.run" should {
-    "be able to run `ls` successfully" in {
+    "be able to run `date` successfully" in {
       File.usingTemporaryDirectory("sample") { sourceDir =>
-        val cmd = "ls " + sourceDir.pathAsString
+        val cmd = "date"
         ExternalCommand.run(cmd, sourceDir.pathAsString) should be a Symbol("success")
       }
     }
   }
 
   "ExternalCommand.runMultiple" should {
-    "be able to run multiple `ls` invocations successfully" in {
+    "be able to run multiple `date` invocations successfully" in {
       File.usingTemporaryDirectory("sample") { sourceDir =>
         File.usingTemporaryFile(parent = Some(sourceDir)) { _ =>
-          val cmd    = "ls " + sourceDir.pathAsString + " && " + "ls " + sourceDir.pathAsString
+          val cmd    = "date && date"
           val result = ExternalCommand.runMultiple(cmd, sourceDir.pathAsString)
           result match {
             case Success(value) => value.split("\n").length shouldBe 2
