d191d20a97fc09adb2f9c96eba332fecedb0816a
Prabhu Subramanian <prabhu@appthreat.com>
2023-12-14 22:53:09 +0000

    Simple hashing without python
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/utils/Fingerprinting.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/utils/Fingerprinting.scala
@@ -1,25 +1,13 @@
 package io.shiftleft.semanticcpg.utils
 
-import me.shadaj.scalapy.py
-import me.shadaj.scalapy.py.SeqConverters
-import py.PyQuote
 import me.shadaj.scalapy.interpreter.CPythonInterpreter
+import me.shadaj.scalapy.py
 
-import scala.util.{Failure, Success, Try}
+import java.security.MessageDigest
 
 object Fingerprinting:
 
-    CPythonInterpreter.execManyLines("""
-      |from hashlib import blake2b
-      |
-      |def calculate_hash(content, digest_size = 16):
-      |  h = blake2b(digest_size = digest_size)
-      |  if content and isinstance(content, str):
-      |    content = content.replace("\n", "").replace("\t", "").replace(" ", "")
-      |    h.update(content.encode())
-      |    return h.hexdigest()
-      |  return None
-      |""".stripMargin)
-
-    def calculate_hash(content: String, digest_size: Int = 16): Option[String] =
-        Option(py.Dynamic.global.calculate_hash(content, digest_size).as[String])
+    def calculate_hash(content: String): String =
+        MessageDigest.getInstance("SHA-256")
+            .digest(content.getBytes("UTF-8"))
+            .map("%02x".format(_)).mkString
