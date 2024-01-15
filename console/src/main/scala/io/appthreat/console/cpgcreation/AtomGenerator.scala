ba0b1cca4ab9d4a10ccf31b34030efebef48daa2
prabhu <prabhu@appthreat.com>
2024-01-15 13:27:50 +0000

    Automatically invoke cdxgen while importing code (#65)
    * Automatically invoke cdxgen while importing code
    
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>
    
    * Use dynamic source
    
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>
    
    ---------
    
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- console/src/main/scala/io/appthreat/console/cpgcreation/AtomGenerator.scala
+++ console/src/main/scala/io/appthreat/console/cpgcreation/AtomGenerator.scala
@@ -14,16 +14,30 @@ case class AtomGenerator(
   sliceMode: String = "reachables",
   slicesFile: String = "reachables.slices.json"
 ) extends CpgGenerator:
-    private lazy val command: String = "atom"
+    private lazy val command: String       = sys.env.getOrElse("ATOM_CMD", "atom")
+    private lazy val cdxgenCommand: String = sys.env.getOrElse("CDXGEN_CMD", "cdxgen")
 
     /** Generate an atom for the given input path. Returns the output path, or None, if no CPG was
       * generated.
       */
     override def generate(inputPath: String, outputPath: String = "app.atom"): Try[String] =
+        // If there is no bom.json file in the root directory, attempt to automatically invoke cdxgen
+        val bomPath = File(inputPath) / "bom.json"
+        if !bomPath.exists then
+            val cdxLanguage = language.toLowerCase().replace("src", "")
+            val arguments = Seq(
+              "-t",
+              cdxLanguage,
+              "--deep",
+              "-o",
+              (File(inputPath) / "bom.json").pathAsString,
+              inputPath
+            )
+            runShellCommand(cdxgenCommand, arguments)
         val arguments = Seq(
           sliceMode,
           "-s",
-          slicesFile,
+          (File(inputPath) / slicesFile).pathAsString,
           "--output",
           (File(inputPath) / outputPath).pathAsString,
           "--language",
@@ -31,6 +45,7 @@ case class AtomGenerator(
           inputPath
         ) ++ config.cmdLineParams
         runShellCommand(command, arguments).map(_ => (File(inputPath) / outputPath).pathAsString)
+    end generate
 
     override def isAvailable: Boolean = true
 
