0c15224beac203e94dc3a41d6b4277ea5d9565bd
Prabhu Subramanian <prabhu@appthreat.com>
2025-07-23 10:29:02 +0100

    Fix conda tos issue
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- ci/conda-install.sh
+++ ci/conda-install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 export PATH=/opt/miniconda3/bin:${PATH}:
-conda config --set always_yes yes --set changeps1 no
 pip install -U pip poetry
+conda config --set always_yes yes --set changeps1 no
 conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
 conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
 conda install -c conda-forge networkx -y
